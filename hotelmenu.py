2d29c1e4373ac24b103b2f3f324f6b10b1d239e4
Anju Swami <138124161+anjuswami25@users.noreply.github.com>
2024-12-28 17:40:55 +0530

    Update hotelmenu.py

--- hotelmenu.py
+++ hotelmenu.py
@@ -8,22 +8,29 @@ menu = {
 
 print("Welcome to our Restaurant")
 print("pizza Rs90\npasta Rs50\nBurger Rs70\nsalad Rs90\ncoffee Rs80")
-order_total = 0
+order_total = 0  # Initialize the total
 
-item_1 = input("Enter the name of item you want to order = ")
-if item_1 in menu:
-    order_total += menu[item_1]
-    print(f"Your item {item_1} has been added to your order")
+# Normalize input to lowercase for case-insensitivity
+item_1 = input("Enter the name of item you want to order = ").strip().lower()
+if item_1 in map(str.lower, menu.keys()):  # Check case-insensitive match
+    for key in menu:  # Find the actual key in the dictionary
+        if key.lower() == item_1:
+            order_total += menu[key]
+            print(f"Your item {key} has been added to your order")
+            break
 else:
     print(f"Order item {item_1} is not available yet")
 
 another_order = input("Do you want to add another item? (Yes/No): ").strip().lower()
 if another_order == "yes":
-    item_2 = input("Enter the name of second item = ")
-    if item_2 in menu:
-        order_total += menu[item_2]
-        print(f"Item {item_2} has been added to your order")
+    item_2 = input("Enter the name of second item = ").strip().lower()
+    if item_2 in map(str.lower, menu.keys()):  # Check case-insensitive match
+        for key in menu:  # Find the actual key in the dictionary
+            if key.lower() == item_2:
+                order_total += menu[key]
+                print(f"Item {key} has been added to your order")
+                break
     else:
         print(f"Order item {item_2} is not available")
-
 print(f"The total amount for your order is Rs{order_total}")
+
